d89c6f6295ec08cda5cd14529bd4cd00ad47abe3
acierto <bogdannechiporenko@gmail.com>
2019-06-10 10:07:25 +0200

    added tablelist section

--- src/client/src/routes/index.ts
+++ src/client/src/routes/index.ts
@@ -1,14 +1,8 @@
 // @material-ui/icons
 import Dashboard from '@material-ui/icons/Dashboard';
-import Person from '@material-ui/icons/Person';
-import LibraryBooks from '@material-ui/icons/LibraryBooks';
-import BubbleChart from '@material-ui/icons/BubbleChart';
-import LocationOn from '@material-ui/icons/LocationOn';
-import Notifications from '@material-ui/icons/Notifications';
-import Unarchive from '@material-ui/icons/Unarchive';
-import Language from '@material-ui/icons/Language';
 // core components/views for Admin layout
 import DashboardPage from '../views/Dashboard/Dashboard';
+import TableList from '../views/TableList/TableList';
 
 const dashboardRoutes = [
     {
@@ -17,6 +11,13 @@ const dashboardRoutes = [
         icon: Dashboard,
         component: DashboardPage,
         layout: '/admin'
+    },
+    {
+        path: "/table",
+        name: "Table List",
+        icon: "content_paste",
+        component: TableList,
+        layout: "/admin"
     }
 ];
 
